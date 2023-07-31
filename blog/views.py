38c591fcf43c126c3cbec40dcebb5c857c3d22c6
Dante <dante1201@mail.ru>
2023-07-31 22:06:06 +0300

    Задание №3 выполнено

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, UpdateView, DetailView, DeleteView
 from pytils.translit import slugify
 
@@ -7,10 +7,14 @@ from blog.models import Article
 
 
 class BlogCreateView(CreateView):
+    """
+    Создает новый экземпляр модели "Статья"
+    """
     model = Article
     fields = ('title', 'body', 'preview',)
     success_url = reverse_lazy('blog:list')
 
+    # Динамически формируем slug name
     def form_valid(self, form):
         if form.is_valid():
             new_mat = form.save()
@@ -21,14 +25,26 @@ class BlogCreateView(CreateView):
 
 
 class BlogListView(ListView):
+    """
+    Выводит все статьи на страницу
+    """
     model = Article
+
+    # Выводит только активные статьи
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(publication=True)
+        return queryset
 
 
 class BlogUpdateView(UpdateView):
+    """
+    Позволяет изменить существующие статьи
+    """
     model = Article
     fields = ('title', 'body', 'preview',)
-    success_url = reverse_lazy('blog:list')
 
+    # Динамически формируем slug name
     def form_valid(self, form):
         if form.is_valid():
             new_mat = form.save()
@@ -37,11 +53,28 @@ class BlogUpdateView(UpdateView):
 
         return super().form_valid(form)
 
+    # После обнавления перенаправляет на статью
+    def get_success_url(self):
+        return reverse('blog:view', args=[self.kwargs.get('pk')])
+
 
 class BlogDetailView(DetailView):
+    """
+    Выводит статью целиком
+    """
     model = Article
+
+    # Счетчик просмотров
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 
 class BlogDeleteView(DeleteView):
+    """
+    Удаляет элемент
+    """
     model = Article
     success_url = reverse_lazy('blog:list')
