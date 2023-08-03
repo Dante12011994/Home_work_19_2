65ecd7c2b2321177092cca0821daaddf0bd419ee
Dante <dante1201@mail.ru>
2023-08-03 13:34:28 +0300

    Реализовали CRUD для модели Product, использовали форму и валидацию

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import main, contact, ProductCreateView, ProductListView
+from catalog.views import main, contact, ProductCreateView, ProductListView, ProductDetailView, ProductUpdateView, \
+    ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', main, name='main'),  # Страница "Главная"
     path('contact', contact, name='contact'),
-    path('products', ProductListView.as_view(), name='products'),  # Страница "Контакты"
+    path('products', ProductListView.as_view(), name='products'),
     path('create-product/', ProductCreateView.as_view(), name='create_product'),
+    path('products/detail/<int:pk>/', ProductDetailView.as_view(), name='detail_product'),
+    path('edit/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
 ]
